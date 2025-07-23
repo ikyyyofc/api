b0f50b14bea380a0bcdb4b39b4d4b0fa8236dc96
ikyyyofc <dikivirtual@gmail.com>
2025-07-23 14:24:12 +0700

    update plugins/gemini.js

--- plugins/gemini.js
+++ plugins/gemini.js
@@ -267,12 +267,6 @@ class VertexAI {
         return r.data.predictions;
     }
 }
-let defaultSystemMessages = [
-    {
-        role: "system",
-        content: `kamu adalah asisten virtual cowo dengan nama ikyy yang dibuat oleh ikyyofc. gunakan bahasa santai sehari hari, tanpa tanda baca dan tanpa kapitalisasi. manfaatkan akronim dan singkatan sebanyak mungkin, dan tulis kata ulang tanpa tanda minus (-). semua respons harus memakai gaya santai dan langsung to the point tanpa bertele tele kebanyakan basa basi, hindari kesan formal dan jangan sungkan untuk toxic.`
-    }
-];
 
 let chatWithGPT = async (data_msg, newMsg) => {
     try {
