f3be679743d7390aa3580593776f45dea13aab49
ikyyyofc <dikivirtual@gmail.com>
2025-07-23 08:42:13 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 // index.js
 const PluginBasedAPI = require('./server');
-const userPlugin = require('./plugins/userPlugin');
-const productPlugin = require('./plugins/productPlugin');
 
 // Inisialisasi API
 const api = new PluginBasedAPI();
 
-// Menambahkan plugin
-api.addPlugin(userPlugin);
-api.addPlugin(productPlugin);
+// Secara otomatis memuat semua plugin dari folder plugins
+api.loadPlugins();
 
 // Menjalankan server
-const PORT = process.env.PORT || 7860;
+const PORT = process.env.PORT || 3000;
 api.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
   console.log('Plugins yang aktif:', api.plugins);
