928031205294c75288a11ffc2ac5c1747d046d12
ikyyyofc <dikivirtual@gmail.com>
2025-07-23 11:58:57 +0700

    update plugins/trendtiktok.js

---
+++ plugins/trendtiktok.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+function router(app, routes = [], pluginName) {
+    routes.push({
+        plugin: pluginName,
+        endpoints: [
+            {
+                method: "GET",
+                path: "/trendtiktok",
+                description: "Get Tiktok Viral"
+            }
+        ]
+    });
+
+    app.get("/trendtiktok", async (req, res) => {
+        if (!req.query.region) {
+            return res.status(400).json({
+                status: false,
+                error: "region parameter is required"
+            });
+        }
+
+        const trend = await axios.post(
+            "https://tikwm.com/api/feed/list",
+            "region=" + req.query.region
+        );
+
+        res.json(trend);
+    });
+}
+
+module.exports = router;
