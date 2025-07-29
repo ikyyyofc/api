6fe50e7da35b29906a25c42ebedd5a1b8bfcd13e
ikyyyofc <dikivirtual@gmail.com>
2025-07-29 09:38:59 +0700

    update plugins/chatgpt.js

--- plugins/chatgpt.js
+++ plugins/chatgpt.js
@@ -23,7 +23,7 @@ function router(app, routes = [], pluginName) {
 
         const resp = await chatai(req.body.user, {
             system_prompt: req.body.system,
-            model: "claude-3.7"
+            model: "o3"
         });
 
         res.json({
